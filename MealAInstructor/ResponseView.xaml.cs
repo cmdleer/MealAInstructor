82356f80fc4550cde31b68f8153974023401416f
Artur Leer <code@artur-leer.de>
2024-06-22 12:06:38 +0200

    Add views; ResponseView.xaml, UserInputView.xaml

---
+++ MealAInstructor/ResponseView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MealAInstructor;
+
+public partial class ResponseView : UserControl
+{
+    public ResponseView()
+    {
+        InitializeComponent();
+    }
+}
---
+++ MealAInstructor/UserInputView.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace MealAInstructor;
+
+public partial class UserInputView : UserControl
+{
+    public UserInputView()
+    {
+        InitializeComponent();
+    }
+}
