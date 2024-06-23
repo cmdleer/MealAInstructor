29cc34ca19ac0348ffb49f3a4d5ea418c858fb88
Artur Leer <code@artur-leer.de>
2024-06-23 10:32:49 +0200

    Add input properties to UserInputViewModel.cs

--- MealAInstructor/ViewModels/UserInputViewModel.cs
+++ MealAInstructor/ViewModels/UserInputViewModel.cs
@@ -2,5 +2,40 @@
 
 public class UserInputViewModel : ViewModelBase
 {
-    
+    private string _ingredient;
+    private string _difficulty;
+    private int _portions;
+
+    public string Ingredient
+    {
+        get => _ingredient;
+        set
+        {
+            if (value == _ingredient) return;
+            _ingredient = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public string Difficulty
+    {
+        get => _difficulty;
+        set
+        {
+            if (value == _difficulty) return;
+            _difficulty = value;
+            OnPropertyChanged();
+        }
+    }
+
+    public int Portions
+    {
+        get => _portions;
+        set
+        {
+            if (value == _portions) return;
+            _portions = value;
+            OnPropertyChanged();
+        }
+    }
 }
