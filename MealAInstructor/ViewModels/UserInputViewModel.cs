cbac0029ca2076ff9d0bdd4182e0c19637ec18b3
Artur Leer <code@artur-leer.de>
2024-06-25 19:39:11 +0200

    Apply Fody in UserInputViewModel.cs

--- MealAInstructor/ViewModels/UserInputViewModel.cs
+++ MealAInstructor/ViewModels/UserInputViewModel.cs
@@ -5,54 +5,10 @@ namespace MealAInstructor.ViewModels;
 
 public class UserInputViewModel : ViewModelBase
 {
-    private string _ingredient = null!;
-    private string _difficulty = null!;
-    private string _addedIngredients = null!;
-    private int _portions;
-
-    public string Ingredient
-    {
-        get => _ingredient;
-        set
-        {
-            if (value == _ingredient) return;
-            _ingredient = value;
-            OnPropertyChanged(nameof(_ingredient));
-        }
-    }
-
-    public string Difficulty
-    {
-        get => _difficulty;
-        set
-        {
-            if (value == _difficulty) return;
-            _difficulty = value;
-            OnPropertyChanged(nameof(_difficulty));
-        }
-    }
-
-    public int Portions
-    {
-        get => _portions;
-        set
-        {
-            if (value == _portions) return;
-            _portions = value;
-            OnPropertyChanged(nameof(_portions));
-        }
-    }
-
-    public string AddedIngredients
-    {
-        get => _addedIngredients;
-        set
-        {
-            if (value == _addedIngredients) return;
-            _addedIngredients = value;
-            OnPropertyChanged(nameof(_addedIngredients));
-        }
-    }
+    public string Ingredient { get; set; }
+    public string Difficulty { get; set; }
+    public string AddedIngredients { get; set; }
+    public int Portions { get; set; }
     
     public ICommand EnterCommand { get; set; }
 
@@ -63,6 +19,5 @@ public class UserInputViewModel : ViewModelBase
 
     private void OnEnter()
     {
-        
     }
 }
